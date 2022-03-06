2134e3ce8bc2ba4aa367ddaa357fea32e7f36d5a
muskmelon <1013830051@qq.com>
2022-03-06 09:37:59 +0800

    change zuul to gateway

---
+++ api-gateway/src/main/java/com/muskmelon/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.muskmelon.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 基于SpringCloud-gateway路由
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
